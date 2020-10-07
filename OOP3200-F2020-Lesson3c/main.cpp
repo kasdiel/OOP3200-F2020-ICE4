fc92d78f3ed8ddb71c69aab4ebc9ed62b2e997aa
Kristopher Hankey <kasdiel@outlook.com>
2020-10-07 16:48:47 -0400

    Added implementation in main (test rig, etc)

--- OOP3200-F2020-Lesson3c/main.cpp
+++ OOP3200-F2020-Lesson3c/main.cpp
@@ -2,45 +2,67 @@
 #include <iostream>
 
 #include "GameObject.h"
+#include <vector>
+
+static void BuildGameObjects(std::vector<GameObject*>& game_objects, const int num = 1)
+{
+	for (int count = 0; count < num; ++count)
+	{
+		int id;
+		std::cout << "Enter the gameObject ID: ";
+		std::cin >> id;
+		Vector2D point;
+		std::cout << "Enter the gameObject Position (x, y): ";
+		std::cin >> point;
+		std::cout << "\n--------------------------------------------------------------" << std::endl;
+		std::cout << "You Entered " << id << " for the gameObject's ID" << std::endl;
+		std::cout << "You Entered " << point << " for the gameObject's Position" << std::endl;
+		std::cout << "--------------------------------------------------------------\n" << std::endl;
+		GameObject* gameObject = new GameObject(id, point);
+		game_objects.push_back(gameObject);
+	}
+}
+
+static void CompareGameObjects(GameObject* object1, GameObject* object2)
+{
+	std::cout << std::fixed << std::setprecision(3);
+	std::cout << "Magnitude of first gameObject is: " << object1->GetPosition().GetMagnitude() << std::endl;
+	std::cout << "Magnitude of second gameObject is: " << object2->GetPosition().GetMagnitude() << std::endl;
+	std::cout << "Distance between first gameObject and second gameObject is: " << Vector2D::Distance(object1->GetPosition(), object2->GetPosition()) << std::endl;
+	std::cout << "--------------------------------------------------------------\n" << std::endl;
+
+	std::cout << "The angle (in degrees) from the first gameObject to the second gameObject is: " << Vector2D::SignedAngle(object1->GetPosition(), object2->GetPosition()) << std::endl;
+
+	std::cout << "--------------------------------------------------------------\n" << std::endl;
+	std::cout << "First gameObject Details: " << std::endl;
+	std::cout << object1->ToString() << std::endl;
+
+	std::cout << "Second gameObject Details: " << std::endl;
+	std::cout << object2->ToString() << std::endl;
+}
 
 int main()
 {
+	std::vector<GameObject*> gameObjects;
 	
-	int id1;
-	std::cout << "Enter the first gameObject ID: ";
-	std::cin >> id1;
-	Vector2D point1;
-    std::cout << "Enter the first point (x, y): ";
-	std::cin >> point1;
-	std::cout << "\n--------------------------------------------------------------" << std::endl;
-	std::cout << "You Entered " << point1 << " for the first gameObject" << std::endl;
-	std::cout << "--------------------------------------------------------------\n" << std::endl;
-	GameObject gameObject1(id1, point1);
-
-	Vector2D point2;
-	int id2;
-	std::cout << "Enter the second gameObject ID: ";
-	std::cin >> id2;
-	std::cout << "Enter the second point (x, y): ";
-	std::cin >> point2;
-	std::cout << "\n--------------------------------------------------------------" << std::endl;
-	std::cout << "You Entered " << point2 << " for the second gameObject" << std::endl;
-	std::cout << "--------------------------------------------------------------\n" << std::endl;
-	GameObject gameObject2(id2, point2);
-
-	std::cout << std::fixed << std::setprecision(3);
-	std::cout << "Magnitude of first gameObject is: " << gameObject1.GetPosition().GetMagnitude() << std::endl;
-	std::cout << "Magnitude of second gameObject is: " << gameObject2.GetPosition().GetMagnitude() << std::endl;
-	std::cout << "Distance between first gameObject and second gameObject is: " << Vector2D::Distance(gameObject1.GetPosition(), gameObject2.GetPosition()) << std::endl;
+	int num_of_GO;
+	std::cout << "How many gameObjects do you need?: ";
+	std::cin >> num_of_GO;
 	std::cout << "--------------------------------------------------------------\n" << std::endl;
 
-	std::cout << "The angle (in degrees) from the first gameObject to the second gameObject is: " << Vector2D::SignedAngle(gameObject2.GetPosition(), gameObject2.GetPosition()) << std::endl;
-	
-	std::cout << "--------------------------------------------------------------\n" << std::endl;
-	std::cout << "First gameObject Details: " << std::endl;
-	std::cout << gameObject1.ToString() << std::endl;
+	BuildGameObjects(gameObjects, num_of_GO);
 
-	std::cout << "Second gameObject Details: " << std::endl;
-	std::cout << gameObject2.ToString() << std::endl;
+	int index1;
+	std::cout << "What is the First Object index: ";
+	std::cin >> index1;
+	std::cout << "--------------------------------------------------------------\n" << std::endl;
+
+	int index2;
+	std::cout << "What is the Second Object index: ";
+	std::cin >> index2;
+	std::cout << "--------------------------------------------------------------\n" << std::endl;
+
+	CompareGameObjects(gameObjects[index1], gameObjects[index2]);
+
 }
 
